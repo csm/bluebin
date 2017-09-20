a80f0e4be7775d42a36ebb3252d8d474aaec72e2
Casey Marshall <casey.s.marshall@gmail.com>
2017-09-20 14:40:57 -0700

    Release tasks, snapshot version.

--- project.clj
+++ project.clj
@@ -1,7 +1,16 @@
-(defproject com.github.csm/bluebin "0.1.0-alpha1"
+(defproject com.github.csm/bluebin "0.1.0-SNAPSHOT"
   :description "Recyclable record types."
   :url "https://github.com/csm/bluebin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.netty/netty-common "4.1.6.Final"]])
+                 [io.netty/netty-common "4.1.6.Final"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
