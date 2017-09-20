82cb6b1f378ca2de15e06c4fcd3df83dc8b55b78
Casey Marshall <casey.s.marshall@gmail.com>
2017-09-20 14:41:24 -0700

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/bluebin "0.1.0"
+(defproject com.github.csm/bluebin "0.1.1-SNAPSHOT"
   :description "Recyclable record types."
   :url "https://github.com/csm/bluebin"
   :license {:name "Eclipse Public License"
